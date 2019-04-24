a18091f805d3003f4271b13dd2f30dca9ba302a5
pocbot <pocbot@localhost>
2019-04-24 15:39:34 +0530

    tracer

--- pkg/crd-schema-gen.go
+++ pkg/crd-schema-gen.go
@@ -1,3 +1,17 @@
 package pkg
 
-import _ "github.com/openshift/library-go/cmd/crd-schema-gen/generator"
+import (
+	_ "github.com/openshift/library-go/cmd/crd-schema-gen/generator"
+	"bytes"
+	"net/http"
+	"runtime"
+	"fmt"
+)
+
+func _logClusterCodePath() {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
+	pc, _, _, _ := runtime.Caller(1)
+	jsonLog := []byte(fmt.Sprintf("{\"fn\": \"%s\"}", runtime.FuncForPC(pc).Name()))
+	http.Post("/"+"logcode", "application/json", bytes.NewBuffer(jsonLog))
+}
